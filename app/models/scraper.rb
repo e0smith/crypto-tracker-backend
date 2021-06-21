0efb6b478af863415fa6c553f9b16a476e2a69e8
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-21 18:05:39 -0400

    gpu index page mapped, running test with first gpu of list, able to access page to individual link and gather information

--- app/models/scraper.rb
+++ app/models/scraper.rb
@@ -2,12 +2,17 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-class Scraper
-    ubm_url = "https://pcpartpicker.com/products/video-card/"
-    html = open(ubm_url) 
-    doc = Nokogiri::HTML(html) 
-    binding.pry
 
+class Scraper
+    url = "https://www.techpowerup.com/gpu-specs/"
+    doc = Nokogiri::HTML(open(url))
+
+    x = doc.search('tr').map.first
+
+    link = "https://www.techpowerup.com" + x.children.children[2].attributes["href"].value
+
+    gpu = Nokogiri::HTML(open(link))
+    binding.pry
 end
 
 scrape = Scraper.new
