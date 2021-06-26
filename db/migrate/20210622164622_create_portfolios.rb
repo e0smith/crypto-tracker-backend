6943dcc0973a8bbc9cb8c80b8fd6b86c49e40261
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-26 16:14:35 -0400

    portfolio param balance changed to quantity

--- db/migrate/20210622164622_create_portfolios.rb
+++ db/migrate/20210622164622_create_portfolios.rb
@@ -2,7 +2,7 @@ class CreatePortfolios < ActiveRecord::Migration[6.1]
   def change
     create_table :portfolios do |t|
       t.string :name
-      t.string :balance
+      t.integer :quantity
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_06_22_164622) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "name"
-    t.string "balance"
+    t.integer "quantity"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
