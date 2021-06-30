00e0afc60d5eea90312b93f21c69dae57730d732
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-30 15:42:44 -0400

    portfolio nested route within users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :cryptos
-  resources :portfolios
+  
+  resources :users do
+    resources :portfolios
+  end
+  # resources :portfolios
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
