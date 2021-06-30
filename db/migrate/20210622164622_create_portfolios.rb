6d1756c82c83b61dffe58ccc5af13976e39bffc7
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-06-30 15:42:29 -0400

    users added

--- db/migrate/20210622164622_create_portfolios.rb
+++ db/migrate/20210622164622_create_portfolios.rb
@@ -2,7 +2,8 @@ class CreatePortfolios < ActiveRecord::Migration[6.1]
   def change
     create_table :portfolios do |t|
       t.string :name
-      t.integer :quantity
+      t.integer :user_id
+      t.float :quantity
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_164622) do
+ActiveRecord::Schema.define(version: 2021_06_30_001001) do
 
   create_table "cryptos", force: :cascade do |t|
     t.string "name"
@@ -38,7 +38,15 @@ ActiveRecord::Schema.define(version: 2021_06_22_164622) do
 
   create_table "portfolios", force: :cascade do |t|
     t.string "name"
-    t.integer "quantity"
+    t.integer "user_id"
+    t.float "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
