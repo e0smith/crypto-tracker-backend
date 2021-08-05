2fd8ad7ee13196529fdc42a124f31d928ef6ac38
Edward Smith Silvia <esmithsilvia@gmail.com>
2021-08-05 15:24:17 -0400

    user controller update uncommented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  # before_action :set_user, only: [:show, :update, :destroy]
+  before_action :set_user, only: [:show, :update, :destroy]
 
   # GET /users
   def index
@@ -25,13 +25,13 @@ class UsersController < ApplicationController
   end
 
   # PATCH/PUT /users/1
-  # def update
-  #   if @user.update(user_params)
-  #     render json: @user
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
 
   # DELETE /users/1
   def destroy
